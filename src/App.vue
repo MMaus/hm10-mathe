b9526d9a0404040770e26adc65c34e7a8b5a88f4
Moritz Maus <h.moritz.maus@gmail.com>
2020-07-12 21:16:58 +0200

    create mental math training site

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,105 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="jumbotron">
+      <h3>Kopfrechnen - Malaufgaben</h3>
+    </div>
+    <Aufgabe v-for="agf in aufgaben_mal" :key="agf.idx" :aufgabe="agf.txt" :ergebnis="agf.ergebnis" />
+
+    <div class="jumbotron">
+      <h3>Kopfrechnen - Plusaufgaben</h3>
+    </div>
+    <Aufgabe v-for="agf in aufgaben_plus" :key="agf.idx" :aufgabe="agf.txt" :ergebnis="agf.ergebnis" />
+
+    <div class="jumbotron">
+      <h3>Kopfrechnen - Minusaufgaben</h3>
+    </div>
+    <Aufgabe v-for="agf in aufgaben_minus" :key="agf.idx" :aufgabe="agf.txt" :ergebnis="agf.ergebnis" />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+import Aufgabe from "./components/Aufgabe.vue";
+
+const oneToTen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const baseMul = Math.floor(Math.random() * 9) + 2;
+let idx = 1;
+
+function shuffle<T>(array: Array<T>): Array<T> {
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+class Rechenaufgabe {
+  txt: string;
+  ergebnis: number;
+  idx: number;
+  constructor(txt: string, ergebnis: number) {
+    this.txt = txt;
+    this.ergebnis = ergebnis;
+    this.idx = idx;
+    idx = idx + 1;
+  }
+}
+
+function malAufgabe(nr: number): Rechenaufgabe {
+  const ergebnis = nr * baseMul;
+  const aufgabenText: string =
+    Math.random() > 0.5
+      ? " " + nr + " * " + baseMul
+      : " " + baseMul + " * " + nr;
+  return new Rechenaufgabe(aufgabenText, ergebnis);
+}
+
+const malAufgaben = shuffle(oneToTen).map(nr => malAufgabe(nr));
+
+function plusAufgabe(limit: number): Rechenaufgabe {
+  const ergebnis = Math.round(Math.random() * limit);
+  const summand1 = Math.round(Math.random() * ergebnis);
+  const summand2 = ergebnis - summand1;
+  const aufgabenText: string = summand1 + " + " + summand2;
+  return new Rechenaufgabe(aufgabenText, ergebnis);
+}
+
+function minusAufgabe(limit: number): Rechenaufgabe {
+  const minuend = Math.round(Math.random() * limit);
+  const subtrahend = Math.round(Math.random() * minuend);
+  const ergebnis = minuend - subtrahend;
+  const aufgabenText: string = minuend + " - " + subtrahend;
+  return new Rechenaufgabe(aufgabenText, ergebnis);
+}
+
+const plusAufgabenLimit = [100, 100, 100];
+const plusAufgaben = plusAufgabenLimit.map(nr => plusAufgabe(nr));
+
+const minusAufgabenLimit = [100, 100, 100];
+const minusAufgaben = minusAufgabenLimit.map(nr => minusAufgabe(nr));
 
 @Component({
   components: {
-    HelloWorld,
-  },
+    Aufgabe
+  }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  aufgaben_mal = malAufgaben;
+  aufgaben_plus = plusAufgaben;
+  aufgaben_minus = minusAufgaben;
+}
 </script>
 
 <style>
---
+++ src/components/Aufgabe.vue
@@ -0,0 +1,99 @@
+<template>
+  <div
+    class="row border p-1 m-1 aufgabenbg align-items-center justify-content-center"
+    v-bind:class="{'bg-warning' : isWarn(), 'bg-success' : isSuccess()}"
+  >
+    <!-- <div class="border col-sm-3 p-1 m-1 aufgabenbg"  -->
+    <p class="col">{{ info }}</p>
+    <div class="input-group mb-2 p-1">
+      <label class="lead p-2">{{aufgabe}}</label>
+      <input
+        type="text"
+        class="form-control"
+        id="eingabe"
+        ref="eingabe"
+        v-on:keyup.enter="checkResult()"
+        tabindex="1"
+        placeholder="Ergebnis?"
+      />
+      <div class="input-group-append">
+        <button
+          tabindex="5"
+          class="btn btn-primary"
+          type="button"
+          @click="checkResult()"
+        >Pr&uuml;fen</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Ref } from "vue-property-decorator";
+
+@Component
+export default class Aufgabe extends Vue {
+  @Prop() private aufgabe!: string;
+  @Prop() private ergebnis!: number;
+
+  @Ref("eingabe") eingabe!: HTMLInputElement;
+
+  richtig = false;
+  berechnet = false;
+  info = "";
+  letzteEingabe = "";
+
+  checkResult(): void {
+    console.log("result is " + this.eingabe.value);
+    this.berechnet = true;
+    const eingabeStr = this.eingabe.value;
+    console.log("Eingabe = " + eingabeStr + ", trimmed: " + eingabeStr.trim());
+    const eingabeNr = Number(this.eingabe.value.trim());
+    this.richtig = eingabeNr == this.ergebnis;
+    this.letzteEingabe = eingabeStr;
+    if (!this.richtig) {
+      this.info = this.letzteEingabe + " ist leider nicht richtig - versuche es nochmal!";
+      this.eingabe.value="";
+    } else {
+      this.info = "richtig!";
+    }
+    console.log(
+      "Richtig = " +
+        this.richtig +
+        ", weil " +
+        eingabeNr +
+        " ?= " +
+        this.ergebnis
+    );
+  }
+
+  isWarn(): boolean {
+    return !this.richtig && this.berechnet;
+  }
+
+  isSuccess(): boolean {
+    return this.richtig && this.berechnet;
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.aufgabenbg {
+  background-color: antiquewhite;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
